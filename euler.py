42bffe1b17f293f4262687a53d38dc06d7670f64
Jordi <ripavifeviricoco@gmail.com>
2013-01-03 19:30:56 +0100

    Update euler.py

--- euler.py
+++ euler.py
@@ -1,5 +1,5 @@
 # test de primaritat simple.
-def isprime(p):
+def primer(p):
 	p = abs(p)
 	if p == 1:
 		return False
@@ -8,3 +8,19 @@ def isprime(p):
 		if p%i == 0:
 			return False
 	return True
+
+def factor(n):
+	arrel = int(n**0.5) + 1
+	fact = []
+	i = 2
+	while i < arrel:
+		if not(primer(i)):
+			i += 1
+		elif n%i == 0:
+			fact.append(i)
+			n = n//i
+		else:
+			i += 1
+	if n != 1:
+		fact.append(n)
+	return fact
